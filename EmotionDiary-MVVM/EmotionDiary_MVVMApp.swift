8e59fec7954064c6d0c64c051aa3993a4e358b19
Jun <wnsgur4092@gmail.com>
2022-09-27 23:27:03 +1000

    Initial Commit

---
+++ EmotionDiary-MVVM/EmotionDiary_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionDiary_MVVMApp.swift
+//  EmotionDiary-MVVM
+//
+//  Created by JunHyuk Lim on 27/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct EmotionDiary_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
