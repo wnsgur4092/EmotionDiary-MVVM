eae3ffe84bd59df0b94b3c937e58d0c39d92f2a6
Jun <wnsgur4092@gmail.com>
2022-09-29 22:47:44 +1000

    [Update] DiaryListView
    - Feat Calendar header
    - Feat NavigationLink to DiaryDetailsView

---
+++ EmotionDiary-MVVM/DiaryDetails/DiaryDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  DiaryDetailsView.swift
+//  EmotionDiary-MVVM
+//
+//  Created by JunHyuk Lim on 29/9/2022.
+//
+
+import SwiftUI
+
+struct DiaryDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DiaryDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiaryDetailsView()
+    }
+}
--- EmotionDiary-MVVM/DiaryList/DiaryListView.swift
+++ EmotionDiary-MVVM/DiaryList/DiaryListView.swift
@@ -10,8 +10,6 @@ import SwiftUI
 struct DiaryListView: View {
     //MARK: - PROPERTIES
     
-    
-    
     @StateObject var vm : DiaryListViewModel
     
     let layout : [GridItem] = [
@@ -24,25 +22,74 @@ struct DiaryListView: View {
     
     //MARK: - BODY
     var body: some View {
-        LazyVGrid(columns: layout) {
-
-            ForEach(vm.keys, id: \.self) { key in
-                Section {
-                    let items = vm.dic[key] ?? []
-                    let ordredItems = items.sorted(by: { $0.date < $1.date})
-                    ForEach(ordredItems) { item in
-                        MoodDiaryCell(diary: item)
-                            .frame(height: 50)
-                    }
-                } header: {
-                    Text(key)
-                }
-
-            } //: LOOP
-        } //: VGRID
+        NavigationView {
+            VStack{
+                ScrollView(.vertical, showsIndicators: true) {
+                    LazyVGrid(columns: layout) {
+                        ForEach(vm.keys, id: \.self) { key in
+                            Section {
+                                let items = vm.dic[key] ?? []
+                                let ordredItems = items.sorted(by: { $0.date < $1.date})
+                                ForEach(ordredItems) { item in
+                                    NavigationLink {
+                                        DiaryDetailsView()
+                                    } label: {
+                                        MoodDiaryCell(diary: item)
+                                            .frame(height: 50)
+                                    } //: LINK
+                                } //: LOOP
+                            } header: {
+                                Text(formattedSectionTitle(key))
+                                    .font(.system(size: 30, weight: .black))
+                            } //: SECTION
+                            .frame(height: 60)
+                            .padding()
+                        } //: LOOP
+                    }//: VGRID
+                }//: SCROLLVIEW
+                HStack{
+                    Button {
+                        print("New Item Button Tapped")
+                    } label: {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30)
+                    } //: BUTTON
+                    .frame(width: 80, height: 80)
+                    .foregroundColor(.white)
+                    .background(.pink)
+                    .cornerRadius(40)
+                } //: HSTACK
+                
+            }
+            .navigationTitle("Emotion Diary")
+        }//: NAVIGATION
     }
 }
 
+//MARK: - EXTENSION
+
+extension DiaryListView {
+    private func formattedSectionTitle(_ id: String) -> String {
+        let dateComponents = id
+            .components(separatedBy: " - ")
+            .compactMap{ Int($0) }
+        guard let year = dateComponents.first, let month = dateComponents.last else {
+            return id
+        }
+        
+        let calendar = Calendar(identifier: .gregorian)
+        let dateComponent = DateComponents(calendar: calendar, year: year, month: month)
+        let date = dateComponent.date!
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM yyyy"
+        return formatter.string(from: date)
+    }
+}
+
+//MARK: - PREVIEW
 struct DiaryListView_Previews: PreviewProvider {
     static var previews: some View {
         DiaryListView(vm: DiaryListViewModel())
